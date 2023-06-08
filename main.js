8e310526cc89d5fc26b3081d97b90945c81951ba
Aliva17 <135979336+Aliva17@users.noreply.github.com>
2023-06-08 15:47:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { create, createReportList } from './modules/canvas.js';
-import { name, draw, reportArea, reportPerimeter } from './modules/square.js';
-import randomSquare from './modules/square.js';
+import { create, createReportList } from 'https://aliva17.github.io/aliva.github.io/modules/canvas.js';
+import { name, draw, reportArea, reportPerimeter } from 'https://aliva17.github.io/aliva.github.io/modules/square.js';
+import randomSquare from 'https://aliva17.github.io/aliva.github.io/modules/square.js';
 
 let myCanvas = create('myCanvas', document.body, 480, 320);
 let reportList = createReportList(myCanvas.id);
